1eaceac378b5e6089742b94470a07f7bb4c69861
Cedric de Radzitzky <cderadzitzky@gmail.com>
2016-08-08 18:13:13 +0200

    longuest word game 2

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def game
+
+    @grid = Array.new(9) { ('A'..'Z').to_a[rand(26)] }
+    @start_time = Time.now
+
+
+  end
+
+  def score
+    @input = params[:input]
+    start_time = params[:start_time].to_datetime
+
+    end_time = Time.now
+    @time_result = (end_time - start_time)
+  end
+end
